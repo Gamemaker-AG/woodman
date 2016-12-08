c557ce15b33b09b017c4be0406b3209c56154b5c
Hans Ole Hatzel <hhatzel@gmail.com>
2016-12-08 22:23:09 +0100

    More indentation fixed.

--- src/main_menu.lua
+++ src/main_menu.lua
@@ -3,20 +3,20 @@ local main_menu = {}
 local bg
 
 function main_menu.load()
-  bg = love.graphics.newImage('assets/img/main_menu.png')
+    bg = love.graphics.newImage('assets/img/main_menu.png')
 end
 
 function main_menu.keypressed(key)
-  if key == 'return' then
-    current_state = game
-  end
+    if key == 'return' then
+        current_state = game
+    end
 end
 
 function main_menu.draw()
-  love.graphics.draw(bg, 0, 0)
-  love.graphics.setColor(0, 0, 0)
-  love.graphics.print('Press enter to play', 50, love.graphics.getHeight()-50)
-  love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(bg, 0, 0)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print('Press enter to play', 50, love.graphics.getHeight()-50)
+    love.graphics.setColor(255, 255, 255)
 end
 
 return main_menu
