c6cf7aa0d838366da9a937cbeeda0b7eecd6d107
Rafael Epplee <raffomania@gmail.com>
2017-01-18 19:12:48 +0100

    clean up server

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+.PHONY: run server all
+
 run:
 	love .
+
+server:
+	cd ./server && npm start
--- server/index.js
+++ server/index.js
@@ -80,7 +80,7 @@ console.log(__dirname)
 
 read();
 
-var server = app.listen(80, function(){
+var server = app.listen(8080, function(){
     var host = server.address().address
     var port = server.address().port
 
